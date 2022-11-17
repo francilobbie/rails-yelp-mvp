bc0ad72e341330393a0d679ee1182b07568d08dc
franci-lobbie lalane <francilobbie.lalane@gmail.com>
2022-11-17 10:29:16 +0100

    My rails yelp app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+Faker::Config.locale = 'fr-CA'
+
+
+puts "Cleaning database..."
+
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+20.times do
+  category = %w(french japanese italian chinese belgian)
+  Restaurant.create(name: Faker::JapaneseMedia::Naruto.character, address: Faker::Address.full_address, category: category.sample, phone_number: Faker::PhoneNumber.cell_phone)
+end
+puts "Finished!"
